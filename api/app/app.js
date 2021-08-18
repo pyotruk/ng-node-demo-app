af56eeb14fc9280734fe1db485a4d3327299709e
Vladimir Ivanenko <hi@vldmr.me>
2021-08-18 15:39:19 +0300

    docker-compose works for api

--- api/app/app.js
+++ api/app/app.js
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv'
 import server from './server'
-import config from './../config'
 import log4js from 'log4js'
+
+dotenv.config()
 const log = log4js.getLogger('app')
 
-server.listen(config.port, function () {
+server.listen(process.env.NODE_PORT, function () {
     log.info(`Server listening on port  ${server.address().port}`)
 })
--- api/app/server.js
+++ api/app/server.js
@@ -1,12 +1,9 @@
 import express from 'express'
 import bodyparser from 'body-parser'
 import cors from 'cors'
-import fs from 'fs'
-import https from 'https'
 import http from 'http'
 import log4js from 'log4js'
 import routes from './routes'
-import config from './../config'
 
 const env = process.env.NODE_ENV
 const isDev = env === 'dev' || env === 'test'
@@ -17,7 +14,7 @@ log4js.configure({
 })
 
 const log = log4js.getLogger('server')
-log.warn('Environment set to [' + env + ']')
+log.info('Environment was set to [' + env + ']')
 
 
 
@@ -29,16 +26,5 @@ app.use(cors())
 routes.configure(app)
 
 export default (() => {
-    if (config.https) {
-        log.warn('HTTPS is using')
-
-        return https.createServer({
-            key: fs.readFileSync(__dirname + '/cert/key.pem'),
-            cert: fs.readFileSync(__dirname + '/cert/cert.pem')
-
-        }, app)
-
-    } else {
-        return http.createServer(app)
-    }
+    return http.createServer(app)
 })()
