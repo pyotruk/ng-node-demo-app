ccd27bebedd774cd378b66809a88981ebca248f0
Vladimir Ivanenko <hi@vldmr.me>
2022-10-26 00:43:31 +0400

    warmup

--- api/app/routes.ts
+++ api/app/routes.ts
@@ -1,12 +1,22 @@
-import Note from "./modules/note";
-import {Express} from "express";
-import sequelize from "./modules/sequelize";
 import log4js, {Logger} from "log4js";
+import dotenv from "dotenv";
+import {Sequelize} from "sequelize";
+import {Express} from "express";
+import Note from "./modules/note";
 
 const log: Logger = log4js.getLogger("routes");
 
+dotenv.config();
+
+const DB_URI = `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}/${process.env.MYSQL_DATABASE}`;
+const sequelize = new Sequelize(DB_URI);
+
 export default {
     configure: async (app: Express): Promise<void> => {
+
+        sequelize.authenticate().catch((err) => {
+            log.fatal(`Failed to connect to DB_URI - ${DB_URI}`, err);
+        });
 
         // creates tables if don't exist, but doesn't drop any
         await sequelize.sync({force: false});
--- db-init/create-user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'globaldots'@'%' IDENTIFIED WITH mysql_native_password BY '1q2w3e4r';
-GRANT ALL PRIVILEGES ON globaldots_code_challenge.* TO 'globaldots'@'%';
-FLUSH PRIVILEGES;
